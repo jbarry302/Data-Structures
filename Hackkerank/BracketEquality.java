904c8e0b87a54f8e6ffb7a04e8660689f2883143
jbarry302 <jbarry302@gmail.com>
2022-11-25 00:31:04 +0800

    added bracket equality solution

---
+++ Hackkerank/BracketEquality.java
@@ -0,0 +1,58 @@
+/**
+ * @(#)BracketEquality.java
+ * 
+ * Check if given bracket expr is equal or not.
+ *
+ * @author github.om/jbarry302
+ * @version 1.00 2022/11/25
+ */
+
+import java.util.Stack;
+
+public class BracketEquality {
+
+    public static void main(String args[]) {
+        Stack<Character> ss  = new Stack<Character>();
+        String e = "(({()})))";
+		
+		for(String input : e.split("")) {
+            char c = input.charAt(0);
+            switch(c) {
+                case '(': 
+                case '{': 
+                case '[': {
+                    ss.push(c);
+                    break;
+                }
+                default: {
+                    if(!ss.isEmpty()) {
+                        //System.out.println("peekval: " + ss.peek() + " = c: " + c);
+                        if(isPartner(ss.peek(), c)) {
+                            ss.pop();
+                        } else {
+                            ss.push(c);
+                        }
+                    } else ss.push(c);
+                    break;
+                }
+            }
+		}
+        
+        System.out.println(ss.isEmpty());
+    }
+    
+    public static boolean isPartner(char x, char y) {
+        if(x == '{') {
+            return y == '}';
+        }
+        if(x == '(') {
+            return y == ')';
+        }
+        if(x == '[') {
+            return y == ']';
+        }
+        return false;
+    }
+    
+    
+}
